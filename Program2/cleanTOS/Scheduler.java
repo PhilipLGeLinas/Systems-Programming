d79a8c06be2b5bd0b90212039a1c1e3674a9b208
Philip <philip.gelinas@wsu.edu>
2019-10-05 19:32:50 -0700

    Part 2 successfully implemented with 3 queues and 2 queue counters.

--- Program2/cleanTOS/Scheduler.java
+++ Program2/cleanTOS/Scheduler.java
@@ -7,17 +7,20 @@ public class Scheduler extends Thread {
     private Vector queue_1;
     private Vector queue_2;
 
+    private int queue_1_counter;
+    private int queue_2_counter;
+    private boolean restart;
+
     // private Vector queue;
     private int timeSlice;
 
     private static final int Q0_TIME_SLICE = 500;
-    private static final int Q1_TIME_SLICE = 1000;
 
-    // New data added to p161 
+    // New data added to p161
     private boolean[] tids; // Indicate which ids have been used
     private static final int DEFAULT_MAX_THREADS = 10000;
 
-    // A new feature added to p161 
+    // A new feature added to p161
     // Allocate an ID array, each element indicating if that id has been used
     private int nextId = 0;
 
@@ -27,7 +30,7 @@ public class Scheduler extends Thread {
             tids[i] = false;
     }
 
-    // A new feature added to p161 
+    // A new feature added to p161
     // Search an available thread ID and provide a new thread with this ID
     private int getNewTid() {
         for (int i = 0; i < tids.length; i++) {
@@ -41,7 +44,7 @@ public class Scheduler extends Thread {
         return -1;
     }
 
-    // A new feature added to p161 
+    // A new feature added to p161
     // Return the thread ID and set the corresponding tids element to be unused
     private boolean returnTid(int tid) {
         if (tid >= 0 && tid < tids.length && tids[tid] == true) {
@@ -51,7 +54,7 @@ public class Scheduler extends Thread {
         return false;
     }
 
-    // A new feature added to p161 
+    // A new feature added to p161
     // Retrieve the current thread's TCB from the queue
     public TCB getMyTcb() {
         Thread myThread = Thread.currentThread(); // Get my thread object
@@ -63,10 +66,26 @@ public class Scheduler extends Thread {
                     return tcb;
             }
         }
+        synchronized (queue_1) {
+            for (int i = 0; i < queue_1.size(); i++) {
+                TCB tcb = (TCB) queue_1.elementAt(i);
+                Thread thread = tcb.getThread();
+                if (thread == myThread) // if this is my TCB, return it
+                    return tcb;
+            }
+        }
+        synchronized (queue_2) {
+            for (int i = 0; i < queue_2.size(); i++) {
+                TCB tcb = (TCB) queue_2.elementAt(i);
+                Thread thread = tcb.getThread();
+                if (thread == myThread) // if this is my TCB, return it
+                    return tcb;
+            }
+        }
         return null;
     }
 
-    // A new feature added to p161 
+    // A new feature added to p161
     // Return the maximal number of threads to be spawned in the system
     public int getMaxThreads() {
         return tids.length;
@@ -74,27 +93,36 @@ public class Scheduler extends Thread {
 
     public Scheduler() {
         timeSlice = Q0_TIME_SLICE;
-        Vector queue_0 = new Vector();
-        Vector queue_1 = new Vector();
-        Vector queue_2 = new Vector();
+        queue_0 = new Vector();
+        queue_1 = new Vector();
+        queue_2 = new Vector();
+        queue_1_counter = 0;
+        queue_2_counter = 0;
+        restart = false;
         initTid(DEFAULT_MAX_THREADS);
     }
 
     public Scheduler(int quantum) {
         timeSlice = quantum;
-        Vector queue_0 = new Vector();
-        Vector queue_1 = new Vector();
-        Vector queue_2 = new Vector();
+        queue_0 = new Vector();
+        queue_1 = new Vector();
+        queue_2 = new Vector();
+        queue_1_counter = 0;
+        queue_2_counter = 0;
+        restart = false;
         initTid(DEFAULT_MAX_THREADS);
     }
 
-    // A new feature added to p161 
+    // A new feature added to p161
     // A constructor to receive the max number of threads to be spawned
     public Scheduler(int quantum, int maxThreads) {
         timeSlice = quantum;
-        Vector queue_0 = new Vector();
-        Vector queue_1 = new Vector();
-        Vector queue_2 = new Vector();
+        queue_0 = new Vector();
+        queue_1 = new Vector();
+        queue_2 = new Vector();
+        queue_1_counter = 0;
+        queue_2_counter = 0;
+        restart = false;
         initTid(maxThreads);
     }
 
@@ -143,37 +171,120 @@ public class Scheduler extends Thread {
         while (true) {
             try {
                 // get the next TCB and its thread
-                if (queue_0.size() == 0)
-                    continue;
-                TCB currentTCB = (TCB) queue_0.firstElement();
-                if (currentTCB.getTerminated() == true) {
-                    queue_0.remove(currentTCB);
-                    returnTid(currentTCB.getTid());
-                    continue;
-                }
-                current = currentTCB.getThread();
-                if (current != null) {
-                    if (current.isAlive())
-                        // current.setPriority(4);
-                        current.resume();
-                    else {
-                        // Spawn must be controlled by Scheduler
-                        // Scheduler must start a new thread
-                        // current.setPriority(4);
-                        current.start();
+                if (queue_0.size() > 0) {
+                    TCB currentTCB = (TCB) queue_0.firstElement();
+                    if (currentTCB.getTerminated() == true) {
+                        queue_0.remove(currentTCB);
+                        returnTid(currentTCB.getTid());
+                        continue;
                     }
-                }
-
-                schedulerSleep();
-                // System.out.println("* * * Context Switch * * * ");
-
-                synchronized (queue_0) {
-                    if (current != null && current.isAlive()) {
-                        // current.setPriority(2);
-                        current.suspend();
+                    current = currentTCB.getThread();
+                    if (current != null) {
+                        if (current.isAlive())
+                            // current.setPriority(4);
+                            current.resume();
+                        else {
+                            // Spawn must be controlled by Scheduler
+                            // Scheduler must start a new thread
+                            // current.setPriority(4);
+                            current.start();
+                        }
                     }
-                    queue_0.remove(currentTCB); // rotate this TCB to the end
-                    queue_0.add(currentTCB);
+
+                    schedulerSleep();
+
+                    synchronized (queue_0) {
+                        if (current != null && current.isAlive()) {
+                            // current.setPriority(2);
+                            current.suspend();
+                        }
+                        queue_0.remove(currentTCB); // rotate this TCB to the end
+                        queue_1.add(currentTCB);
+                    }
+                } else if (queue_1.size() > 0) {
+                    TCB currentTCB = (TCB) queue_1.firstElement();
+                    if (currentTCB.getTerminated() == true) {
+                        queue_1.remove(currentTCB);
+                        returnTid(currentTCB.getTid());
+                        continue;
+                    }
+                    current = currentTCB.getThread();
+                    if (current != null) {
+                        if (current.isAlive())
+                            // current.setPriority(4);
+                            current.resume();
+                        else {
+                            // Spawn must be controlled by Scheduler
+                            // Scheduler must start a new thread
+                            // current.setPriority(4);
+                            current.start();
+                        }
+                    }
+
+                    while (queue_1_counter < 2) {
+                        schedulerSleep();
+                        queue_1_counter++;
+                        if (queue_0.size() > 0) {
+                            restart = true;
+                            break;
+                        }
+                    }
+                    if (restart) {
+                        restart = false;
+                        continue;
+                    }
+                    queue_1_counter = 0;
+
+                    synchronized (queue_1) {
+                        if (current != null && current.isAlive()) {
+                            // current.setPriority(2);
+                            current.suspend();
+                        }
+                        queue_1.remove(currentTCB); // rotate this TCB to the end
+                        queue_2.add(currentTCB);
+                    }
+                } else if (queue_2.size() > 0) {
+                    TCB currentTCB = (TCB) queue_2.firstElement();
+                    if (currentTCB.getTerminated() == true) {
+                        queue_2.remove(currentTCB);
+                        returnTid(currentTCB.getTid());
+                        continue;
+                    }
+                    current = currentTCB.getThread();
+                    if (current != null) {
+                        if (current.isAlive())
+                            // current.setPriority(4);
+                            current.resume();
+                        else {
+                            // Spawn must be controlled by Scheduler
+                            // Scheduler must start a new thread
+                            // current.setPriority(4);
+                            current.start();
+                        }
+                    }
+
+                    while (queue_2_counter < 4) {
+                        schedulerSleep();
+                        queue_2_counter++;
+                        if (queue_0.size() > 0 || queue_1.size() > 0) {
+                            restart = true;
+                            break;
+                        }
+                    }
+                    if (restart) {
+                        restart = false;
+                        continue;
+                    }
+                    queue_2_counter = 0;
+
+                    synchronized (queue_2) {
+                        if (current != null && current.isAlive()) {
+                            // current.setPriority(2);
+                            current.suspend();
+                        }
+                        queue_2.remove(currentTCB); // rotate this TCB to the end
+                        queue_2.add(currentTCB);
+                    }
                 }
             } catch (NullPointerException e3) {
             }
